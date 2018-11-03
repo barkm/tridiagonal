a73160f0b36ee042d87e454da59e61c5da85cbd7
Patrik Barkman <barkm@kth.se>
2018-11-03 10:41:17 +0100

    Updated README and example

--- examples/example.cpp
+++ examples/example.cpp
@@ -1,4 +1,6 @@
 //
+//        LHS       X   =  RHS
+//
 //  | b0  c0  0  | |x1|   |d0|
 //  | a1  b1  c1 | |x2| = |d1|
 //  | 0   a2  b2 | |x3|   |d2|
@@ -9,41 +11,40 @@
 
 int main(){
 
-    Eigen::Matrix<double, 6, 6> M;
+    Eigen::Matrix<double, 6, 6> LHS;
 
-    M <<    0, 3,  1, 2,  0, 0,
-            2, 1,  4, 5,  0, 0,
+    LHS <<    0, 3,  1, 2,  0, 0,
+              2, 1,  4, 5,  0, 0,
 
-            6, 7,  1, 2,  4, 5,
-            1, 3,  9, 0,  2, 3,
+              6, 7,  1, 2,  4, 5,
+              1, 3,  9, 0,  2, 3,
 
-            0, 0,  2, 1,  5, 2,
-            0, 0,  3, 7,  9, 3;
+              0, 0,  2, 1,  5, 2,
+              0, 0,  3, 7,  9, 3;
 
-
-    Eigen::Matrix<double, 6, 1> D;
-    D <<    0,
-            2,
-            3,
-            1,
-            -9,
-            2;
+    Eigen::Matrix<double, 6, 1> RHS;
+    RHS <<    0,
+              2,
+              3,
+              1,
+              -9,
+              2;
 
     Eigen::Matrix2d a1, a2, b0, b1, b2, c0, c1;
-    b0 = M.block(0, 0, 2, 2);
-    b1 = M.block(2, 2, 2, 2);
-    b2 = M.block(4, 4, 2, 2);
+    b0 = LHS.block(0, 0, 2, 2);
+    b1 = LHS.block(2, 2, 2, 2);
+    b2 = LHS.block(4, 4, 2, 2);
 
-    c0 = M.block(0, 2, 2, 2);
-    c1 = M.block(2, 4, 2, 2);
+    c0 = LHS.block(0, 2, 2, 2);
+    c1 = LHS.block(2, 4, 2, 2);
 
-    a1 = M.block(2, 0, 2, 2);
-    a2 = M.block(4, 2, 2, 2);
+    a1 = LHS.block(2, 0, 2, 2);
+    a2 = LHS.block(4, 2, 2, 2);
 
     Eigen::Vector2d d0, d1, d2;
-    d0 = D.block(0, 0, 2, 1);
-    d1 = D.block(2, 0, 2, 1);
-    d2 = D.block(4, 0, 2, 1);
+    d0 = RHS.block(0, 0, 2, 1);
+    d1 = RHS.block(2, 0, 2, 1);
+    d2 = RHS.block(4, 0, 2, 1);
 
     vector<Eigen::Matrix2d> diagonal = {b0, b1, b2};
     vector<Eigen::Matrix2d> lower_diagonal = {a1, a2};
@@ -55,12 +56,12 @@ int main(){
                                                                       upper_diagonal,
                                                                       rhs);
 
-    Eigen::Matrix<double, 6, 1> tridiagonal_solution;
-    tridiagonal_solution.block(0, 0, 2, 1) = solution[0];
-    tridiagonal_solution.block(2, 0, 2, 1) = solution[1];
-    tridiagonal_solution.block(4, 0, 2, 1) = solution[2];
+    Eigen::Matrix<double, 6, 1> tridiagonal_X;
+    tridiagonal_X.block(0, 0, 2, 1) = solution[0];
+    tridiagonal_X.block(2, 0, 2, 1) = solution[1];
+    tridiagonal_X.block(4, 0, 2, 1) = solution[2];
 
-    Eigen::Matrix<double, 6, 1> eigen_solution = M.fullPivHouseholderQr().solve(D);
+    Eigen::Matrix<double, 6, 1> eigen_X = LHS.fullPivHouseholderQr().solve(RHS);
 
-    assert(tridiagonal_solution.isApprox(eigen_solution));
+    assert(tridiagonal_X.isApprox(eigen_X));
 }
